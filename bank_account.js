c74f1babee1c3749ae7dc6fa7de5bbb578e90c0d
Prananda Yogathama Pribadi <pranandayoga21@gmail.com>
2024-06-12 11:56:52 +0800

    initial init

---
+++ bank_account.js
@@ -0,0 +1,23 @@
+let saldo = 0;
+
+function tambahSaldo() {
+	let num = parseInt(prompt(`Saldo saat ini ${saldo} \nMasukkan saldo : `));
+	if (num) {
+		let result = (saldo += num);
+		alert(`Saldo kamu saat ini : Rp ${result}`);
+	} else {
+		alert("Kamu belum memasukkan saldo");
+	}
+	return saldo;
+}
+
+function kurangSaldo() {
+	let num = parseInt(prompt(`Saldo saat ini ${saldo} \nMasukkan saldo : `));
+	if (num) {
+		let result = (saldo -= num);
+		alert(`Saldo kamu saat ini : Rp ${result}`);
+	} else {
+		alert("Kamu belum memasukkan saldo");
+	}
+	return saldo;
+}
